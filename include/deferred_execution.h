5e35d9f6c4dcd397754963c98f6659243889ed31
Sarun Rattanasiri <midnight_w@gmx.tw>
2019-10-06 00:07:25 +0700

    add library implementations

---
+++ include/deferred_execution.h
@@ -0,0 +1,29 @@
+// Copyright (c) 2019 Sarun Rattanasiri
+// under the 3-Clause BSD License
+// https://opensource.org/licenses/BSD-3-Clause
+
+#ifndef __DEFERRED_EXECUTION_H
+#define __DEFERRED_EXECUTION_H
+
+#include <stdint.h>
+
+typedef struct _deferred_execution__config {
+  void (*target)(void);
+} DeferredExecution_Config;
+
+typedef struct _deferred_execution__state {
+  uint8_t deferred;
+} DeferredExecution_State;
+
+#define deferred_execution__init(state) \
+  do {                                  \
+    (state)->deferred = 0;              \
+  } while (0)
+
+void deferred_execution__defer(DeferredExecution_Config* config,
+                               DeferredExecution_State* state);
+
+void deferred_execution__execute(DeferredExecution_Config* config,
+                                 DeferredExecution_State* state);
+
+#endif
---
+++ src/deferred_execution.c
@@ -0,0 +1,21 @@
+// Copyright (c) 2019 Sarun Rattanasiri
+// under the 3-Clause BSD License
+// https://opensource.org/licenses/BSD-3-Clause
+
+#include "deferred_execution.h"
+
+void deferred_execution__defer(DeferredExecution_Config* config,
+                               DeferredExecution_State* state) {
+  state->deferred = 1;
+}
+
+void deferred_execution__execute(DeferredExecution_Config* config,
+                                 DeferredExecution_State* state) {
+  if (!state->deferred) {
+    return;
+  }
+  if (config->target) {
+    config->target();
+  }
+  state->deferred = 0;
+}
